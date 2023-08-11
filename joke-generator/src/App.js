6a318dd1eef31d92ef525621e152adce855203fc
vikkshdotsoni <vikkshdotsoni@gmail.com>
2023-08-11 12:46:04 +0000

    Updated Code

--- joke-generator/src/App.js
+++ joke-generator/src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import store from './components/Store/store';
+import JokeGenerator from './components/JokeGenerator/JokeGenerator';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hiii Hello</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <JokeGenerator/>
+      </div>
+    </Provider>
   );
 }
 
---
+++ joke-generator/src/components/Action/action.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+
+export const REQUEST_JOKE = 'REQUEST_JOKE';
+export const RECEIVE_JOKE = 'RECEIVE_JOKE';
+
+const API_URL = 'https://ide-deacccbceddaeacdaecdcbdacbedad.project.examly.io/proxy/3003';
+
+export const requestJoke = () => ({
+  type: REQUEST_JOKE,
+});
+
+export const receiveJoke = (joke) => ({
+  type: RECEIVE_JOKE,
+  joke,
+});
+
+export const fetchRandomJoke = () => async (dispatch) => {
+  dispatch(requestJoke());
+
+  try {
+    const response = await axios.get(`${API_URL}/jokes/random`);
+    console.log(response.data)
+    dispatch(receiveJoke(response.data));
+  } catch (error) {
+    console.error('Error fetching joke:', error);
+  }
+};
+
+export const fetchProgrammingJoke = () => async (dispatch) => {
+  dispatch(requestJoke());
+
+  try {
+    const response = await axios.get(`${API_URL}/jokes/programming`);
+    console.log(response.data)
+    dispatch(receiveJoke(response.data));
+  } catch (error) {
+    console.error('Error fetching joke:', error);
+  }
+};
+
+export const fetchScienceJoke = () => async (dispatch) => {
+  dispatch(requestJoke());
+
+  try {
+    const response = await axios.get(`${API_URL}/jokes/science`);
+    console.log(response.data)
+    dispatch(receiveJoke(response.data));
+  } catch (error) {
+    console.error('Error fetching joke:', error);
+  }
+};
+
+export const fetchPunsJoke = () => async (dispatch) => {
+    dispatch(requestJoke());
+  console.log('HIII')
+    try {
+      const response = await axios.get(`${API_URL}/jokes/puns`);
+      console.log(response.data)
+      dispatch(receiveJoke(response.data));
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  };
+
+  
+  export const fetchKnockJoke = () => async (dispatch) => {
+    dispatch(requestJoke());
+  
+    try {
+      const response = await axios.get(`${API_URL}/jokes/knockknock`);
+      console.log(response.data)
+      dispatch(receiveJoke(response.data));
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  };
+  
