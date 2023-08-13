9cf88a4cbc4fd0c5fd007d27ff3008f30e19c88a
Tulasi-Chimata <chtulasi1807@gmail.com>
2023-08-13 14:08:00 +0000

    server.js change

--- joke-generator/backend/server.js
+++ joke-generator/backend/server.js
@@ -2,6 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors"); // Import the cors package
 
 // Custom routes
 server.use("/api", router);
@@ -16,6 +17,12 @@ const getRandomJokeByType = (jokeType) => {
 // Define a single route that accepts the joke type as a parameter
 server.get("/:type", (req, res) => {
   const jokeType = req.params.type; // Get the joke type from the URL parameter
+  if (
+    !["puns", "programming", "random", "science", "knockknock"].includes(
+      jokeType
+    )
+  )
+    return res.status(200);
   const joke = getRandomJokeByType(jokeType);
   if (joke) {
     res.json(joke);
@@ -23,6 +30,9 @@ server.get("/:type", (req, res) => {
     res.status(404).json({ error: "Joke type not found" });
   }
 });
+
+// Use CORS middleware to allow cross-origin requests
+server.use(cors());
 
 // Start the server
 server.use(middlewares);
